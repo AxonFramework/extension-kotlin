34844c8f27034764c14e2c0ac962837e3fb1dd05
Hidde Wieringa <hidde@hiddewieringa.nl>
2022-04-16 14:39:51 +0200

    Use surrogate in example. `@Serializer(forClass = ...)` still doesnt work

--- kotlin/src/main/kotlin/org/axonframework/extensions/kotlin/serialization/serializer/ConfigTokenSerializer.kt
+++ kotlin/src/main/kotlin/org/axonframework/extensions/kotlin/serialization/serializer/ConfigTokenSerializer.kt
@@ -2,48 +2,37 @@ package org.axonframework.extensions.kotlin.serialization.serializer
 
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
-import kotlinx.serialization.SerializationException
+import kotlinx.serialization.Serializable
 import kotlinx.serialization.Serializer
-import kotlinx.serialization.builtins.MapSerializer
-import kotlinx.serialization.builtins.serializer
 import kotlinx.serialization.descriptors.SerialDescriptor
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.descriptors.element
-import kotlinx.serialization.encoding.CompositeDecoder.Companion.DECODE_DONE
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
-import kotlinx.serialization.encoding.decodeStructure
-import kotlinx.serialization.encoding.encodeStructure
 import org.axonframework.eventhandling.tokenstore.ConfigToken
+
+@Serializable
+data class ConfigTokenSurrogate(
+    private val config: Map<String, String>
+) {
+    fun toToken() = ConfigToken(config)
+}
+
+fun ConfigToken.toSurrogate() = ConfigTokenSurrogate(config)
 
 @OptIn(ExperimentalSerializationApi::class)
 @Serializer(forClass = ConfigToken::class)
 class ConfigTokenSerializer : KSerializer<ConfigToken> {
-    override val descriptor: SerialDescriptor = buildClassSerialDescriptor("ConfigToken") {
-        element<Map<String, String>>("config")
-    }
+    override val descriptor: SerialDescriptor = ConfigTokenSurrogate.serializer().descriptor
 
-    override fun serialize(encoder: Encoder, value: ConfigToken) {
-        encoder.encodeStructure(descriptor) {
-            encodeSerializableElement(descriptor, 0, MapSerializer(String.serializer(), String.serializer()), value.config)
-        }
-    }
+    override fun serialize(encoder: Encoder, value: ConfigToken) =
+        encoder.encodeSerializableValue(ConfigTokenSurrogate.serializer(), value.toSurrogate())
 
-    override fun deserialize(decoder: Decoder): ConfigToken {
-        return decoder.decodeStructure(descriptor) {
-            var config: Map<String, String>? = null
-
-            loop@ while (true) {
-                when (val index = decodeElementIndex(descriptor)) {
-                    DECODE_DONE -> break@loop
-
-                    0 -> config = decodeSerializableElement(descriptor, 0, MapSerializer(String.serializer(), String.serializer()))
-
-                    else -> throw SerializationException("Unexpected index $index")
-                }
-            }
-
-            ConfigToken(config)
-        }
-    }
+    override fun deserialize(decoder: Decoder): ConfigToken =
+        decoder.decodeSerializableValue(ConfigTokenSurrogate.serializer()).toToken()
 }
+
+// The following does not work, emits compiler errors.
+// Also with the `-Xuse-ir` Kotlin compiler argument (version 1.6.20)
+
+//@OptIn(ExperimentalSerializationApi::class)
+//@Serializer(forClass = ConfigToken::class)
+//object ConfigTokenSerializer
